06772eaee8e44e16b0102b132032229c3b2e9673
carlowensky CHARLOTIN <carlowenskycharlotin@gmail.com>
2023-04-21 16:41:38 -0400

    first commit

--- app.config.js
+++ app.config.js
@@ -1,4 +1,5 @@
-{
+import 'dotenv/config';
+export default {
   "expo": {
     "name": "Ralotchin-Chat",
     "slug": "Ralotchin-Chat",
@@ -9,7 +10,7 @@
     "splash": {
       "image": "./assets/splash.png",
       "resizeMode": "contain",
-      "backgroundColor": "#ffffff"
+      "backgroundColor": "#ffffff",
     },
     "assetBundlePatterns": [
       "**/*"
@@ -25,6 +26,22 @@
     },
     "web": {
       "favicon": "./assets/favicon.png"
+    },
+    extra: {
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID,
+      appId: process.env.APP_ID,
+      "eas": {
+        "projectId": "dd1e262a-1e22-4384-ab99-a2bac4f63391"
+      }
+    },
+    "android": {
+      "package": "com.example.myapp"
     }
   }
 }
+
+
